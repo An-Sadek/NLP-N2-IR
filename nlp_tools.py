ad29a0923cb94a4871d336ba10a0409c4df6b84a
An Hoang Nguyen <nguyenan.sadek@outlook.com>
2025-02-11 22:03:41 +0700

    Xong stemming, nhung chua lam lemmatization

--- nlp_tools.py
+++ nlp_tools.py
@@ -1,21 +1,27 @@
 import nltk
 nltk.download('stopwords')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
 
 import regex as re
 
 from nltk.tokenize import word_tokenize 
 from nltk.corpus import stopwords
 import string
+from nltk.stem import PorterStemmer
 
 
 class Preprocessing:
 
     def __init__(self, corpus: list[str]):
         self.corpus = corpus
-        self.pattern = r"(?:#|@)\w+\s*"
+        self.hashtag_uname_pattern = r"(?:#|@)\w+\s*"
+        self.digit_pattern = r"\S*\d+\S*"
+        self.stemmer = PorterStemmer()
+        self.lemmatizer = WordNetLemmatizer()
 
     def remove_hashtag_username(self):
-        self.corpus = [re.sub(self.pattern, '', text).strip() for text in self.corpus]
+        self.corpus = [re.sub(self.hashtag_uname_pattern, '', text).strip() for text in self.corpus]
 
     def tokenization(self):
         self.corpus = [word_tokenize(sentence) for sentence in self.corpus]
@@ -41,7 +47,27 @@ class Preprocessing:
             ] for sub_list in self.corpus
         ]
         
-    
+    def remove_number(self):
+        self.corpus = [
+            [
+                re.sub(self.digit_pattern, '', text).strip() for text in sub_list
+            ] for sub_list in self.corpus
+        ]
+
+        self.corpus = [
+            [
+                word for word in sub_list if word
+            ] for sub_list in self.corpus
+        ]
+
+    def stemming(self):
+        self.corpus = [
+            [
+                self.stemmer.stem(word) for word in sub_list
+            ] for sub_list in self.corpus
+        ]
+
+    def lemmatization(self):
         
 
     
@@ -49,9 +75,9 @@ if __name__ == "__main__":
     corpus = [
         "The mechanism of pattern recognition in the brain is \
         little known, and it seems to be almost impossible to \
-        reveal it only by conventional physiological experiments. #A", 
+        reveal it only by conventional physiological experiments.", 
         
-        "So, we take a slightly different approach to this. #A @B"]
+        "So, we take a slightly different approach to this. #A @1234 5k Android 4.1"]
     pre_tool = Preprocessing(corpus)
     print("Original")
     print(pre_tool.corpus)
@@ -74,4 +100,12 @@ if __name__ == "__main__":
 
     print("\nRemove stopwords")
     pre_tool.remove_stopwords()
+    print(pre_tool.corpus)
+
+    print("\nRemove num")
+    pre_tool.remove_number()
+    print(pre_tool.corpus)
+
+    print("\nStemming")
+    pre_tool.stemming()
     print(pre_tool.corpus)
---
+++ test.py
@@ -0,0 +1,5 @@
+import nltk
+nltk.download("words")
+
+E_dict = nltk.corpus.words.words()
+print(E_dict)
