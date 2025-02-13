5f26317b18719edf588dce3f4b3aaf2be0c83c0c
An Hoang Nguyen <nguyenan.sadek@outlook.com>
2025-02-13 20:08:40 +0700

    Them simple rnn, ma con cui bap

--- tfML.py
+++ tfML.py
@@ -1,3 +1,42 @@
+import os 
+import math
+
+import numpy as np
+import polars as pl
+
 import tensorflow as tf
 from tensorflow import keras
-from keras import layers
+from keras import layers, utils
+
+
+class SteamDataset(utils.Sequence):
+
+    def __init__(self, path: str, batch_size: int, n_rows: int):
+        super().__init__()
+
+        assert os.path.exists(path), "Duong dan khong ton tai"
+        self.data = pl.read_csv(path)
+        self.batch_size = batch_size
+
+    def __len__(self):
+        return math.ceil(len(self.data) / self.batch_size)
+
+    def __getitem__(self, idx):
+        start_idx = idx
+        end_idx = idx + self.batch_size
+        
+        curr_data = self.data[start_idx: end_idx, "review_text"]
+        curr_label = self.data[start_idx: end_idx, "review_votes"]
+
+        batch_data = np.zeros((len(curr_data),))
+
+
+
+class LogisticRegression(layers.Layer):
+
+    def __init__(self):
+        pass
+
+    def call(self):
+        pass
+
